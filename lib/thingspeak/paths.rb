c40b9961820f648e102eb6f3ba9b45c866038d67
Lee Lawlor <primemod3@gmail.com>
2012-05-30 02:32:37 -0300

    use SSL since write keys may be used in any HTTP requests

--- lib/thingspeak/paths.rb
+++ lib/thingspeak/paths.rb
@@ -1,6 +1,6 @@
 module ThingSpeak
 
-  THINGSPEAK_BASE_URL = "http://api.thingspeak.com"
+  THINGSPEAK_BASE_URL = "https://api.thingspeak.com"
   THINGSPEAK_UPDATE_URL = "#{THINGSPEAK_BASE_URL}/update"
 
 end
